0fc9b54272d09b70db437e2520c54aa5f3f6b6a6
James Quick <james.q.quick@outlook.com>
2019-09-18 15:42:05 -0500

    Updated logging of errors

--- extension.js
+++ extension.js
@@ -1,34 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
-const fs = require("fs");
-const path = require("path");
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "web-boilerplate" is now active!'
-  );
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log(
+        'Congratulations, your extension "web-boilerplate" is now active!'
+    );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "extension.createWebBoilerplate",
-    async function() {
-      if (!vscode.workspace) {
-        return vscode.window.showErrorMessage(
-          "Please open a project folder first"
-        );
-      }
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand(
+        'extension.createWebBoilerplate',
+        async function() {
+            if (!vscode.workspace) {
+                return vscode.window.showErrorMessage(
+                    'Please open a project folder first'
+                );
+            }
 
-      const folderPath = vscode.workspace.workspaceFolders[0].uri
-        .toString()
-        .split(":")[1];
+            const folderPath = vscode.workspace.workspaceFolders[0].uri
+                .toString()
+                .split(':')[1];
 
-      const htmlContent = `<!DOCTYPE html>
+            const htmlContent = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
@@ -43,26 +43,45 @@ function activate(context) {
 </html>
         `;
 
-      fs.writeFile(path.join(folderPath, "index.html"), htmlContent, err => {
-        if (err) {
-          return console.log(err);
+            fs.writeFile(
+                path.join(folderPath, 'index.html'),
+                htmlContent,
+                (err) => {
+                    if (err) {
+                        return console.log(err);
+                        vscode.window.showErrorMessage(
+                            'Failed to create boilerplate files'
+                        );
+                    }
+                    fs.writeFile(path.join(folderPath, 'app.js'), '', (err) => {
+                        if (err) {
+                            return console.log(err);
+                            vscode.window.showErrorMessage(
+                                'Failed to create boilerplate files'
+                            );
+                        }
+                        fs.writeFile(
+                            path.join(folderPath, 'app.css'),
+                            '',
+                            (err) => {
+                                if (err) {
+                                    return console.log(err);
+                                    vscode.window.showErrorMessage(
+                                        'Failed to create boilerplate files'
+                                    );
+                                }
+                                vscode.window.showWarningMessage(
+                                    'Created boilerplate files!'
+                                );
+                            }
+                        );
+                    });
+                }
+            );
         }
-        fs.writeFile(path.join(folderPath, "app.js"), "", err => {
-          if (err) {
-            return console.log(err);
-          }
-          fs.writeFile(path.join(folderPath, "app.css"), "", err => {
-            if (err) {
-              return console.log(err);
-            }
-            vscode.window.showWarningMessage("Created boilerplate files!");
-          });
-        });
-      });
-    }
-  );
+    );
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
